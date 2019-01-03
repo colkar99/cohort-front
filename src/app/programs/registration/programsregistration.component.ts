bd8af7578736a96518735682198e9e65f819f758
colkar99 <colkar99@gmail.com>
2019-01-03 17:42:33 +0530

    new changes updated forprogram registration

--- src/app/programs/registration/programsregistration.component.ts
+++ src/app/programs/registration/programsregistration.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiCommunicationService } from '../../api-communication.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormArray, FormControl,FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-programsregistration',
@@ -7,32 +10,75 @@ import { ApiCommunicationService } from '../../api-communication.service';
   styleUrls: ['./programsregistration.component.css']
 })
 export class ProgramsRegistrationComponent implements OnInit {
- 
-  constructor(private apiService: ApiCommunicationService) { }
+  program_id: number;
+  formSubmitted: boolean = false
+  application_questions: any;
+  application_questions_form: any;
+  startup_application: any;
+  app_ques:any;
+  constructor(private apiService: ApiCommunicationService,
+              private route: ActivatedRoute,
+              private fb: FormBuilder,
+              private router: Router) {
+                 this.program_id = +this.route.snapshot.paramMap.get('id');
+                 this.application_questions_form = this.fb.group({
+                  application_question: new FormArray([])
+                 })
+
+               }
 
   ngOnInit() {
     // this.getAllProgram();
+    this.getApplicationQuestion();
   }
-
-  handleFormSubmit(form){
+  initAppQuesSubmit(form){
     debugger
+    let dum_data = form.value;
+    debugger
+    dum_data.application_question.forEach(element => {
+      let data = {application_ques_response: element, program_id: this.program_id,startup_application_id: this.startup_application.id };
+      this.apiService.postData('create-program-questions-response',JSON.stringify(data))
+      .subscribe(data =>{
+        console.log(data);
+      },error =>{
+        console.log(data)
+        })
+    });
+    this.router.navigate(['/']);
+  }
+ 
+  handleFormSubmit(form){
+    let data = {program_registration: form.value}
+    data.program_registration.program_id = this.program_id;
+    this.apiService.postData('program/startup-registration',JSON.stringify(data))
+    .subscribe(data =>{
+      console.log(data);
+      this.startup_application = data
+      this.formSubmitted = true;
+    },error =>{
+      console.log(error)
+      })
+  }
+  createQuesResponse(element): FormGroup{
+    return this.fb.group({
+      application_question_id: element.id,
+      response: ''
+    });
+  }
+  getApplicationQuestion(){
+    let data = {program_id: this.program_id};
+    this.apiService.postData('show-program-questions',JSON.stringify(data))
+    .subscribe(data => {
+      console.log(data);
+      this.application_questions = data;
+      this.application_questions.forEach(element => {
+        this.app_ques = this.application_questions_form.get('application_question') as FormArray;
+        this.app_ques.push(this.createQuesResponse(element));
+      });
+    },error => {
+      console.log(`Following error has occured: ${error}`);
+    });
   }
   
-//   getAllProgram(){
-//     this.apiService.getDataWithoutAuth('get-list-of-programs')
-//     .subscribe(data => {
-//       this.allPrograms = data;
-//       this.livePrograms = data.all_programs.live;
-//       this.expPrograms = data.all_programs.exp;
-//       debugger
-//       console.log(`All programs: ${this.allPrograms}`);
-//       console.log(`live programs: ${data.all_programs.live}`);
-//       console.log(`exp programs: ${data.all_programs.exp}`);
-
-//     }, error =>{
-//       console.log(`The following error has been occured: ${error}`)
-
-//     })
-//   }
 
 }
