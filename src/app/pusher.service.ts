06297ff77e6783de0f2e7dafa7d361999834dcae
colkar99 <colkar99@gmail.com>
2019-03-18 09:54:12 +0530

    new pusher added

---
+++ src/app/pusher.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable,of } from 'rxjs';
+
+declare const Pusher: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PusherService {
+  pusher: any;
+  channel: any;
+  constructor(private http: HttpClient) {
+    this.pusher = new Pusher(environment.pusher.key, {
+      cluster: environment.pusher.cluster,
+      encrypted: true
+    });
+    this.channel = this.pusher.subscribe('events-channel');
+  }
+  like( num_likes ) {
+    // this.http.post('http://localhost:3120/update', {'likes': num_likes})
+    // .subscribe(data => {});
+    let s_data = {like: num_likes}
+    this.http.post('http://localhost:3000/v1/update',{s_data})
+    .subscribe(data => {console.log(data)});
+  }
+
+}
