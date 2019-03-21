529317dc7e4f86dc213fd4c15fb05acd6fa584bb
colkar99 <colkar99@gmail.com>
2019-03-21 16:05:15 +0530

    asas

--- src/app/sitedashboard/startupby-programs/startupby-programs.component.ts
+++ src/app/sitedashboard/startupby-programs/startupby-programs.component.ts
@@ -81,7 +81,7 @@ export class StartupbyProgramsComponent implements OnInit {
     // })
     // this.newMessage();
     const channel: Channel = this.cableService
-    .cable('http://ec2-54-172-0-213.compute-1.amazonaws.com/news-feed-websocket')
+    .cable('wss://ec2-54-172-0-213.compute-1.amazonaws.com/news-feed-websocket')
     // .cable('http://localhost:3000/news-feed-websocket')
     .channel('NewsFeedsChannel');
     this.subscription = channel.received().subscribe(message => {
